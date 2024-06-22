814e06ea30c67ea95571f7813969d4272ae54bd8
xiaotan-git <tanxiaobuaa@gmail.com>
2024-06-22 15:46:11 -0700

    add SSR decomposition

--- ss_problem.py
+++ ss_problem.py
@@ -1,6 +1,7 @@
 from typing import List
 import itertools
 import numpy as np
+from scipy import linalg
 import control as ct
 
 # sampling rate
@@ -37,13 +38,24 @@ class SSProblem():
     '''
     This class defines data, system model, etc for SSR problem
     '''
-    def __init__(self, Ac, Bc, Cc, Dc,s=2) -> None:
-        A,B,C,D = self.ct_to_dt(Ac, Bc, Cc, Dc, TS)
+    def __init__(self, Ac, Bc, Cc, Dc, s=2, measurements = None, init_from_ct = True,input_exist = True) -> None:
+        if init_from_ct:
+            A,B,C,D = self.ct_to_dt(Ac, Bc, Cc, Dc, TS)
 
-        self.A = A
-        self.B = B
-        self.C = C
-        self.D = D
+            self.A = A
+            self.B = B
+            self.C = C
+            self.D = D
+        else:
+            self.A = Ac
+            self.B = Bc
+            self.C = Cc
+            self.D = Dc
+
+        if not input_exist and measurements is not None:
+            self.y_his = measurements
+        if input_exist and measurements is not None:
+            self.tilde_y_his = measurements
 
         self.n = np.shape(Ac)[0] # dim of states
         self.m = np.shape(Bc)[1] # dim of inputs
@@ -168,9 +180,9 @@ class SecureStateReconst():
         A = ss_problem.A
         for i in range(ss_problem.p):
             Ci = ss_problem.C[i:i+1,:]
-            obser_i = Ci@np.linalg.matrix_power(A,0)
+            obser_i = Ci@linalg.fractional_matrix_power(A,0)
             for t in range(1,ss_problem.tspan):
-                new_row = Ci@np.linalg.matrix_power(A,t)
+                new_row = Ci@linalg.fractional_matrix_power(A,t)
                 obser_i = np.vstack((obser_i,new_row))
             obser_array[:,:,i:i+1] = obser_i.reshape(ss_problem.tspan,ss_problem.n,1)
         self.obser = obser_array
@@ -194,7 +206,9 @@ class SecureStateReconst():
             yi = tilde_yi - fi@u_vec #2d array of dimension (tspan,1)
             yi_list.append(yi)
         
+        # y_his: shape (tspan, p) = [y1 y2 ... yp] for p sensors, and yi is a column vector [yi(0); yi(1); .... yi(tspan-1)] 
         self.y_his = np.hstack(yi_list)
+        # print(f'shape of y_his: {self.y_his.shape}')
 
     def construct_fi(self,i):
         '''
@@ -210,18 +224,10 @@ class SecureStateReconst():
         for t in range(1,tspan):
             fi[t:t+1,:] = right_shift_row_array(fi[t-1:t,:],m)
             # here at the t-th row, t = 1,...,tspan -1, the left most element is Ci A^t B. 
-            fi[t:t+1,0:m] = Ci @ np.linalg.matrix_power(A,t) @ B 
+            fi[t:t+1,0:m] = Ci @ linalg.fractional_matrix_power(A,t) @ B 
         return fi
-
-    def construct_obser_matrix(self,comb):
-        obser_matrix_list = []
-        for i in comb:
-            obser_i = self.obser[:,:,i]
-            obser_matrix_list.append(obser_i)
-        obser_matrix = np.vstack(obser_matrix_list)
-        return obser_matrix
-    
-    def construct_measurement_his(self,comb):
+       
+    def construct_y_his_vec(self,comb):
         measure_vec_list = []
         for i in comb:
             measure_i = self.y_his[:,i:i+1]
@@ -237,15 +243,15 @@ class SecureStateReconst():
         corresp_sensors_list = []
         residuals_list = []
         for comb in self.possible_comb:
-            obser_matrix = self.construct_obser_matrix(comb)
-            measure_vec = self.construct_measurement_his(comb)
+            obser_matrix = self.vstack_comb(self.obser,comb)
+            measure_vec = self.vstack_comb(self.y_his,comb)
 
             # print(f'Observation matrix shape {obser_matrix.shape}, measure_vec shape {measure_vec.shape}')
-            state, residuals, rank, _ = np.linalg.lstsq(obser_matrix,measure_vec,rcond=-1)
+            state, residuals, rank, _ = linalg.lstsq(obser_matrix,measure_vec)
 
             if len(residuals)<1:
                 # print(f'combinations: {comb}')
-                residuals = np.linalg.norm(obser_matrix@state - measure_vec,ord=2)**2
+                residuals = linalg.norm(obser_matrix@state - measure_vec,ord=2)**2
 
             if residuals <error_bound:
                 possible_states_list.append(state)
@@ -271,6 +277,7 @@ class SecureStateReconst():
         return possible_states, corresp_sensors, corresp_sensors_list
     
     def solve(self,error_bound = 1):
+        # Solves for current states
         possible_states, corresp_sensors, corresp_sensors_list = self.solve_initial_state(error_bound)
         current_states_list = []
         for ind in range(possible_states.shape[1]):
@@ -279,6 +286,159 @@ class SecureStateReconst():
             current_states_list.append(curr_state)
         current_states = np.hstack(current_states_list)
         return current_states, corresp_sensors, corresp_sensors_list
+
+    def gen_subssr_data(self,y_his):
+        '''
+        generate data A^(j), C_i, Y_i^j for sensor i = 1,...,p and generalized eigenspace V^j, j = 1,...,r
+        subprob_a, subprob_c, subprob_y = self.gen_subssr_data(y_his)
+        A^(j), C_i, Y_i^j = subprob_a[:,:,j], subprob_c[i,:], subprob_y[:,i,j]
+        '''
+        eigval_a = linalg.eigvals(self.problem.A)
+        unique_eigvals, counts = np.unique(eigval_a,return_counts = True)
+        # total number of subspaces V^j, j = 1,..., r
+        r = len(unique_eigvals)
+        obser_full = self.vstack_comb(self.obser)
+
+        geig_space_list = []
+        gobser_space_list = []
+        for j in range(r):
+            eigval =  unique_eigvals[j]
+            am = counts[j]
+            geigspace = self.gen_eigenspace(eigval,am, self.problem.A)
+            obser_subspace_vec_list = []
+            for k in range(geigspace.shape[1]):
+                obser_subspace_vec = obser_full@geigspace[:,k]
+                obser_subspace_vec = obser_subspace_vec.reshape(-1,1) #N*1 2d array
+                print(f'shape of obser_subspace_vec: {obser_subspace_vec.shape}')
+                obser_subspace_vec_list.append(obser_subspace_vec)
+            obser_subspace = np.hstack(obser_subspace_vec_list)
+            print(f'shape of obser_subspace: {obser_subspace.shape}')
+
+            geig_space_list.append(geigspace)
+            gobser_space_list.append(obser_subspace)
+
+        subprob_a_list = []
+        subprob_y_list = []
+        for j in range(r):
+            # for the generalized eigenspace V^j
+            proj_gs_j = self.construct_proj(geig_space_list,j) # This is P_j: R^n -> V^j
+            proj_obs_j = self.construct_proj(gobser_space_list,j) # This is tilde_P_j: O(R^n) -> O(V^j)
+
+            # construct a,c,y for subspace V^j
+            proj_a_j = proj_gs_j@self.problem.A # 2d array
+            y_his_vec = self.vstack_comb(self.y_his)
+            print(f'shape of proj_obs_j: {proj_obs_j.shape}, shape of y_his_vec: {y_his_vec.shape}')
+            proj_y_his_j_vec =  proj_obs_j@y_his_vec # 2d column array, [y1(0), y1(1), ...,y1(tspan-1), y2(0), ....]
+            subprob_a_list.append(proj_a_j)
+            proj_y_his_j = self.unvstack(proj_y_his_j_vec,self.problem.tspan)
+            subprob_y_list.append(proj_y_his_j)
+
+        subprob_a = np.dstack(subprob_a_list).real
+        subprob_y = np.dstack(subprob_y_list).real
+
+        assert linalg.norm(subprob_a - np.dstack(subprob_a_list))<= 1e-6
+        assert linalg.norm(subprob_y - np.dstack(subprob_y_list))<= 1e-6
+            
+        return subprob_a, self.problem.C, subprob_y
+    
+    @staticmethod
+    def unvstack(array_to_unstack:np.ndarray,row_count_for_a_slice:int):
+        rows = array_to_unstack.shape[0]
+        quotient, remainder = divmod(rows, row_count_for_a_slice) 
+        if remainder !=0:
+            Warning('unable to unvstack the array')
+        temp_lst = []
+        for i in range(quotient):
+            temp_lst.append(array_to_unstack[i*row_count_for_a_slice:(i+1)*row_count_for_a_slice,:])
+        if array_to_unstack.shape[1] == 1:
+            array_unstacked = np.hstack(temp_lst)
+        else:
+            array_unstacked = np.dstack(temp_lst)
+
+        return array_unstacked
+
+    @staticmethod
+    def vstack_comb(array_to_stack:np.ndarray,comb:list = None,axis:int = None):
+        '''
+        vertically stack numpy array that is sliced along **axis** with some **combination**, 
+        default axis: last axis; default comb: full indices    
+
+        a = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5,2])
+        b = a.reshape(2,2,-1)
+        c = SecureStateReconst.vstack_comb(b,comb = [1,2],axis = 2)
+        '''
+        if axis is None:
+            axis = array_to_stack.ndim-1 # last dimension
+        if comb is None:
+            p = array_to_stack.shape[axis]
+            comb = [i for i in range(p)]
+        temp_list = []
+        for i in comb:
+            temp_i = SecureStateReconst.slice_along_axis(array_to_stack,i,axis)
+            if temp_i.ndim == 1:
+                temp_i = temp_i.reshape(-1,1) # so temp_i becomes 2d column array
+            temp_list.append(temp_i)
+        stacked_array = np.vstack(temp_list)
+        return stacked_array
+    
+    @staticmethod
+    def slice_along_axis(arr, j, k):
+        """
+        Extract the j-th slice along the k-th axis of a d-dimensional array.
+
+        Parameters:
+        arr (np.ndarray): The input d-dimensional array.
+        j (int): The index of the slice to extract.
+        k (int): The axis along which to extract the slice.
+
+        Returns:
+        np.ndarray: The extracted slice.
+        """
+        # Create a full slice (:) for each dimension
+        index = [slice(None)] * arr.ndim
+        
+        # Replace the slice for the k-th axis with the specific index j
+        index[k] = j
+        
+        # Convert the list to a tuple and use it to slice the array
+        return arr[tuple(index)]
+
+    @staticmethod
+    def gen_eigenspace(eigval, am, A):
+        '''
+        Calculates generalized eigenspace of A corresp. to eigval
+        am -- algebraic multi
+        '''
+        temp_A = linalg.fractional_matrix_power(A - eigval*np.eye(A.shape[0]), am)
+        generalized_eigenspace = linalg.null_space(temp_A)
+        return generalized_eigenspace
+        
+    @staticmethod
+    def construct_proj(subspace_list,j):
+        '''
+        construct the projection matrix from the whole space to the jth subspace, i.e., 
+        v = sum_j vj, vj = Pj*v, vj \in subspace_j
+        Moreover, we know Pj*spj = spj, Pj*spi = 0 . To calculate Pj, we choose a basis for the whole space 
+        V = (sp1,...,spj,...,spr). Based on [0, ..., spj, ...., 0] = Pj* [sp1, ..., spj, ...., spr] and 
+        V is full column rank, we know
+        ************  Pj = [0, ..., spj, ...., 0] (V^T V)^{-1} V^T   **************
+        '''
+        full_space = np.hstack(subspace_list) #  (sp1, sp2, ..., spr)
+        rank_full_space = np.linalg.matrix_rank(full_space)
+        assert rank_full_space == full_space.shape[1]
+
+        tem_list = []
+        for item in range(len(subspace_list)):
+            subspace = subspace_list[j]
+            if item != j:
+                tem_list.append(np.zeros(subspace.shape))
+            else:
+                tem_list.append(subspace)
+        
+        lhd = np.hstack(tem_list)
+
+        proj_mat = lhd @ linalg.inv(full_space.T @ full_space) @ full_space.T 
+        return proj_mat
 
 if __name__ == "__main__":
     # define system model and measurement model
@@ -316,3 +476,9 @@ if __name__ == "__main__":
             sensors = corresp_sensors[ind,:]
             state = possible_states[:,ind]
             print(f'Identified possible states:{state} for sensors {sensors}')
+
+    # test decompostion method
+    y_his = ssr_solution.y_his
+    subprob_a, subprob_c, subprob_y = ssr_solution.gen_subssr_data(y_his)
+
+    subproblem1 = SSProblem(subprob_a[:,:,0],Bc,Cc,Dc,measurements = subprob_y[:,:,0],input_exist=False)
